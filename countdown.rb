398c1f10ba147aef14625bb81d3c01bf63b9c13b
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-06 12:22:40 -0400

    commit tree e408fd05eb72a1ff829ff783250f3b2482f6c32b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown_with_sleep(integer)
+    while integer > 0
+        puts "#{integer} SECOND(S)!"
+        sleep(1)   
+        integer -= 1
+    end
+end
 
-def countdown
+
+def countdown(integer)
+    while integer > 0
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
